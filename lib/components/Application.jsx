db6562373cb53ca8cee6fe54736061387bb8c6ae
chelle tuerk <michelletuerk@gmail.com>
2017-01-11 09:00:10 -0700

    ready for evalutation

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
-import { scoreWord } from './helpers/scoreWord';
+import  scoreWord  from './helpers/scoreWord';
 require('../style.scss')
 
 export default class Org extends Component {
-  
+  render() {
+    return (
+      <div>
+        Oh Hey
+      </div>
+    )
+  }
 }
+export default Org;
--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import sinon from 'sinon';
 import { shallow, mount, render } from 'enzyme';
 import { expect, assert } from 'chai';
-const App = require('../lib/Application');
+import Application from '../lib/components/Application';
+const scoreWord = require('../lib/components/helpers/scoreWord');
 
-describe('', () => {
-  it('', () => {
-    console.log('Ahem...');  // eslint-disable-line no-console
+describe("Application", () => {
+
+  it('should render as a <div>', () => {
+    const wrapper = shallow(<Application />);
+    assert.equal(wrapper.type(), 'div');
   });
+});
+
+describe ('scoreWord', () => {
+  it('should be a function', () => {
+    assert.isFunction(scoreWord);
+  });
+
+  it('scores an empty word as zero', (text) => {
+    assert.equal(scoreWord('')).toEqual(0);
+  });
+
+  it('scores a letter', () => {
+    assert.equal(scoreWord('a')).toEqual(1);
+  });
+
+  it('scores words with spaces', () => {
+    assert.equal(scoreWord('righteous bro')).toEqual(18);
+  });
+
+  it('scores case insensitive words', () => {
+    assert.equal(scoreWord('SUH')).toEqual(6);
+  });
+
 });
