2cfa8fd1237fdb8b5b44a5220069b05a7a0ad2f3
chelle tuerk <michelletuerk@gmail.com>
2017-01-11 11:01:56 -0700

    took out second export

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import  scoreWord  from './helpers/scoreWord';
 require('../style.scss')
 
-export default class Org extends Component {
+class Org extends Component {
   render() {
     return (
       <div>
